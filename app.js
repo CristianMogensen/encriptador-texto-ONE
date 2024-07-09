0730ac6308b8c458b376e1893ca9da191be34b65
Cristian G. Mogensen <105217957+CristianMogensen@users.noreply.github.com>
2024-07-09 18:25:20 -0300

    Funciones implementadas para encriptar, desencriptar y copiar.

--- app.js
+++ app.js
@@ -96,3 +96,78 @@ function desencriptarTexto (textoEncriptado) {
 // console.log("Texto ingresado: " + textito);
 // let resultado = desencriptarTexto(textito);
 // console.log("Resultado texto desencriptado: " + resultado);
+
+const texto_input = document.getElementById("txt_input");
+const texto_output = document.getElementById("txt_output");
+const texto_mensaje = document.getElementById("mensaje");
+const imagen_munieco = document.getElementById("img_munieco");
+
+function esconderMensajeImagen(texto) {
+    let hayTexto = (texto != "");
+    
+    if (!hayTexto) {
+        texto_mensaje.style.display = "block";
+    } else {
+        texto_mensaje.style.display = "none";
+        imagen_munieco.style.display = "none";
+    }
+
+    return hayTexto;
+}
+
+function click_encriptarTexto() {
+    // Obtengo el texto escrito por el usuario.
+    let texto = texto_input.value;
+    let msj_sinTexto = "Ingresa el texto que deseas encriptar o desencriptar."
+
+    // Si escribio un texto, escondo la imagen del muñeco y el mensaje.
+    let hayTexto = esconderMensajeImagen(texto);
+
+    if (hayTexto) {
+        // Lo encripto y retorno el resultado;
+        let encriptado = encriptarTexto(texto);
+
+        // Modifico el contenido del texto a mostrar.
+        texto_output.value = encriptado;
+    } else {
+        texto_output.value = msj_sinTexto;
+    }
+    
+}
+
+function click_desencriptarTexto() {
+    // Obtengo el texto escrito por el usuario.
+    let texto = texto_input.value;
+    let msj_sinTexto = "Ingresa el texto que deseas encriptar o desencriptar."
+
+    // Si escribio un texto, escondo la imagen del muñeco y el mensaje.
+    let hayTexto = esconderMensajeImagen(texto);
+
+    if (hayTexto) {
+        // Lo encripto y retorno el resultado;
+        let desencriptado = desencriptarTexto(texto);
+
+        // Modifico el contenido del texto a mostrar.
+        texto_output.value = desencriptado;
+    } else {
+        texto_output.value = msj_sinTexto;
+    }
+    
+}
+
+function click_copiarTexto() {
+    // Obtengo el texto escrito por el usuario.
+    let texto_a_copiar = texto_output.value;
+    let texto_escrito = texto_input.value;
+
+    // Verifico que si el usuario no escribió un texto en el input, entonces
+    // no copia el texto del output. Caso contrario, sí.
+    if (texto_escrito != "") {
+        // Copia el texto al portapapeles.
+        navigator.clipboard.writeText(texto_a_copiar).then(function() {
+            console.log("Texto copiado al portapapeles.")
+        }).catch(function(error) {
+            console.error('Error al copiar texto: ', error);
+        });
+    }
+}
